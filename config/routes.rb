43586c4238158b5aa3d8aaf6bcac1bcf0ae70dfe
LWD <valeragh@gmail.com>
2014-08-01 14:56:17 +0300

    Assets precompile for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 StackoverflowMlsdev::Application.routes.draw do
-  root 'static_pages#home'
+  
   resources :posts
 
   resources :users
+
+  root 'static_pages#home'
+  match '/signup',  to: 'users#new',            via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
