bd3ad1026fc780b371009638656a47ef23d010f1
LWD <valeragh@gmail.com>
2014-08-01 17:34:46 +0300

    Edit Flash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Добро пожаловать в Stackerflow(MLSDev)"
+      flash[:success] = "Velcome in Stackerflow(MLSDev)"
       redirect_to @user
     else
       render 'new'
